58f34ada1628375c88b80cc959138affaab01abf
Nuella-Concept <Emmanuelanweke8@gmail.com>
2023-08-30 05:43:33 -0700

    new file added here

---
+++ script.js
@@ -0,0 +1,8 @@
+function show() {
+  let password = document.getElementById("password");
+  if (password.type === "password") {
+    password.type = "text";
+  } else {
+    password.type = "password";
+  }
+}
